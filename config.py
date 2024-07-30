5ca8fa492c5d6ae408b95caf5b6ec0eedce22af5
Shubham Sameliya <shsameliya@bestpeers.com>
2024-07-30 13:55:52 +0530

    docker-compose.yml updste

--- config.py
+++ config.py
@@ -8,10 +8,14 @@ class Config(object):
     TESTING = False
     SECRET_KEY = os.getenv("SECRET_KEY", default="BAD_SECRET_KEY")
 
-    # postgresql
-    
-    # mysql
-    SQLALCHEMY_DATABASE_URI = os.getenv("MYSQL_DATABASE_URI")
+    # # mysql
+    # SQLALCHEMY_DATABASE_URI = os.getenv("MYSQL_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:ROOT@127.0.0.1:5432/flask_db"
+
+    # Postgres configuration
+    # SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:ROOT@db:5432/pytest_2_test"
+    # MySQL configuration
+    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@db:3306/pytest_2_test"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_WITH_GUNICORN = os.getenv("LOG_WITH_GUNICORN", default=False)
@@ -27,8 +31,12 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "TEST_DATABASE_URI", default=os.getenv("TEST_MYSQL_DATABASE_URI")
-    )
+    # SQLALCHEMY_DATABASE_URI = os.getenv(
+    #     "TEST_DATABASE_URI", default=os.getenv("TEST_MYSQL_DATABASE_URI")
+    # )
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:ROOT@127.0.0.1:5432/flask_test_db"
+
+    # SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:ROOT@db_test:5432/pytest_2_pytest"
+
     # SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI',default=f"sqlite:///{os.path.join(BASEDIR, 'instance', 'test.db')}")
     WTF_CSRF_ENABLED = False
